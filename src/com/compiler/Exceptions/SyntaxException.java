4181a835eecf8acc8ab3a77457b7bbcfe414372a
Patrickggwpez <minipatrick@hotmail.com>
2022-04-22 15:38:33 +0200

    Grammar test
    til grammartest

---
+++ src/com/compiler/Exceptions/SyntaxException.java
@@ -0,0 +1,14 @@
+package com.compiler.Exceptions;
+
+public class SyntaxException extends Exception {
+    public SyntaxException() {
+        super("Not supported in AHLL");
+    } {
+
+    }
+    public SyntaxException(String message) {
+        super(message);
+    } {
+
+    }
+}
--- tests/com/tests/GrammarTests.java
+++ tests/com/tests/GrammarTests.java
@@ -8,10 +8,14 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+
+import com.compiler.Exceptions.SyntaxException;
+
 
 public class GrammarTests {
     @Test
@@ -19,18 +23,19 @@ public class GrammarTests {
         String grammarTest = " func loop {} func setup() {}";
         String randomInputs = " func fun1() {func fun2() {}} func fun11() {}";
 
-        CharStream stream = CharStreams.fromString(grammarTest+randomInputs);
-        HlmpLexer lexer = new HlmpLexer(stream);
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        HlmpParser parser = new HlmpParser(tokens);
-        parser.removeErrorListeners();
-        parser.addErrorListener(ThrowingErrorListener.INSTANCE);
-        ParseTree tree = parser.program();
 
 
-        //Assertions.assertDoesNotThrow();
 
+        Assertions.assertThrows(ParseCancellationException.class, () -> {
+            CharStream stream = CharStreams.fromString(grammarTest+randomInputs);
+            HlmpLexer lexer = new HlmpLexer(stream);
+            lexer.removeErrorListeners();
+            lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            HlmpParser parser = new HlmpParser(tokens);
+            parser.removeErrorListeners();
+            parser.addErrorListener(ThrowingErrorListener.INSTANCE);
+            ParseTree tree = parser.program();
+        });
     }
 }
