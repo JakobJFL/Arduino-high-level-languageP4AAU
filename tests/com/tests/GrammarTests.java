745a9a95cb4856230cc267a5144205ee2f4be707
Patrickggwpez <minipatrick@hotmail.com>
2022-04-26 15:37:02 +0200

    More testing

--- tests/com/tests/GrammarTests.java
+++ tests/com/tests/GrammarTests.java
@@ -14,7 +14,7 @@ public class GrammarTests {
     private static String setUpLoop = "proc setup() {} proc loop() {}";
     @Test
     public void GrammarTestAlreadyDeclared() {
-        String randomInputs = setUpLoop + " proc  fun1() {proc  fun1() {}} proc  fun11() {}";
+        String randomInputs = setUpLoop + " proc fun1() {proc  fun1() {}} proc  fun11() {}";
 
         Assertions.assertThrows(AlreadyDeclared.class, () -> {
             compile(randomInputs);
@@ -22,9 +22,25 @@ public class GrammarTests {
     }
     @Test
     public void GrammarTestWideSpaces() {
-        String randomInputs = setUpLoop + " proc  fun1() \n \n \n\n \n {proc  fun1() {}} proc  fun11()\n\n\n\n {}";
+        String randomInputs = setUpLoop + " proc  fun1() \n \n \n\n \n {proc  fun2() {}} proc  fun11()\n\n\n\n {}";
 
         Assertions.assertDoesNotThrow(() -> {
+            compile(randomInputs);
+        });
+    }
+    @Test
+    public void SyntaxGrammarTest () {
+        String randomInputs = setUpLoop + " prog fun1() {proc  fun12() {}} proc  fun11() {}";
+
+        Assertions.assertThrows(SyntaxException.class, () -> {
+            compile(randomInputs);
+        });
+    }
+    @Test
+    public void OperandTest () {
+        String randomInputs = setUpLoop + "";
+
+        Assertions.assertThrows(SyntaxException.class, () -> {
             compile(randomInputs);
         });
     }
