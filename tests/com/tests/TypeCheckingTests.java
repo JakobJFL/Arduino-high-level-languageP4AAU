08688f0b258e81f8b608e34121f9f7c9b51e0d40
knielsen-afk <74406677+knielsen-afk@users.noreply.github.com>
2022-05-10 22:03:11 +0200

    More unittest

--- tests/com/tests/TypeCheckingTests.java
+++ tests/com/tests/TypeCheckingTests.java
@@ -1,13 +1,6 @@
 package com.tests;
 
-import com.compiler.Exceptions.AlreadyDeclared;
 import com.compiler.Exceptions.TypeException;
-import com.compiler.HlmpLexer;
-import com.compiler.HlmpParser;
-import com.compiler.SymbolTbl.Scope;
-import com.compiler.SymbolTbl.SymbolTblListener;
-import com.compiler.TypeCheckerVisitor;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -15,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static com.compiler.Main.compile;
 import static org.junit.Assert.assertTrue;
 
-public class TypeTests {
+public class TypeCheckingTests {
     private static String setUpLoop = " proc setup() {} proc loop() {}";
 
     @ParameterizedTest
--- tests/com/tests/UnitTest.java
+++ tests/com/tests/UnitTest.java
@@ -9,6 +9,11 @@ import org.antlr.v4.runtime.tree.ParseTreeProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.compiler.Main.compile;
+
 public class UnitTest {
 
     @Test
@@ -21,5 +26,83 @@ public class UnitTest {
         // Act & Assert
         symbolTbl.addSymbol(symbol1);
         Assertions.assertThrows(AlreadyDeclared.class, () -> symbolTbl.addSymbol(symbol2));
+    }
+
+    @Test
+    public void getSymbolDefinedInScope_shouldGet() {
+        // Arrange
+        Symbol symbol = new Symbol("test", "1");
+        SymbolTbl symbolTbl = new SymbolTbl();
+        Scope scope = new Scope(null, null);
+
+        // Act
+        scope.addThisSymbol(symbol);
+
+        // Assert
+        Assertions.assertEquals(scope.getThisSymbol("test"), symbol);
+    }
+
+    @Test
+    public void enterScope_shouldCreateNewScope() { //help
+        // Arrange
+        String id = "testFunction";
+        Scope globalScope = new Scope(null, null);
+        Scope currentScope = globalScope;
+        List<Scope> actualScopes = new ArrayList<>();
+        List<Scope> expectedScopes = new ArrayList<>();
+        Scope expectedScope = new Scope(globalScope, "testFunction");
+
+        // Act
+        expectedScopes.add(expectedScope);
+
+        for (Scope s: currentScope.getSubScopes()) {
+            if (s.id == id) {
+                currentScope = s;
+                return;
+            }
+        }
+        Scope scope = new Scope(currentScope, id);
+        currentScope.addSubScope(scope);
+        currentScope = scope;
+        actualScopes.add(currentScope);
+        System.out.println(expectedScopes);
+        System.out.println(actualScopes);
+
+        // Assert
+        Assertions.assertEquals(expectedScopes, actualScopes);
+    }
+
+    @Test
+    public void getDefinedSubScopes_shouldGet() {
+        // Arrange
+        Scope scope = new Scope(null, null);
+        Scope subScope = new Scope(scope, "subScope");
+        Scope subScope2 = new Scope(scope, "subScope2");
+        List<Scope> subScopesList = new ArrayList<>();
+
+        // Act
+        scope.addSubScope(subScope);
+        scope.addSubScope(subScope2);
+
+        subScopesList.add(subScope);
+        subScopesList.add(subScope2);
+
+        // Assert
+        Assertions.assertEquals(scope.getSubScopes(), subScopesList);
+    }
+
+    @Test
+    public void accessGlobalSymbolFromInner() { //nono right?
+        // Arrange
+        Symbol symbol = new Symbol("test", "1");
+        Scope globalScope = new Scope(null, "globalScope");
+        Scope subScope = new Scope(globalScope, "subScope");
+
+        // Act
+        globalScope.addSubScope(subScope);
+        globalScope.addThisSymbol(symbol);
+
+        // Assert
+        Assertions.assertEquals(subScope.parent.getThisSymbol("test"), symbol);
     }
 }
